916525b198216201be0f820d24b05691ff04477b
muster-Wugui <tintinpost28@gmail.com>
2024-11-02 15:11:02 -0700

    Initial Commit

---
+++ 210-Lab 28-Xiao Zhang/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  210-Lab 28-Xiao Zhang
+//
+//  Created by Xiao Zhang on 11/2/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
